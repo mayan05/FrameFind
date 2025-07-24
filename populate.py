95f9d854775a850f008c2fb7911bf250a5458ccc
Mayan Sequeira <mayan.sequeira@gmail.com>
2025-07-24 15:41:24 +0530

    movie table complete

--- populate.py
+++ populate.py
@@ -13,13 +13,15 @@ movies = []
 if not api_key: # Couldnt find the api key
     raise ValueError("TMDB_API_KEY environment variable not set.")
 
+print("\nCollecting the Genres and their ids...")
+
 # Fetch genres
 genre_url = f"{base_url}/genre/movie/list?api_key={api_key}"
 genre_response = requests.get(genre_url).json()
 genres_map = {genre["id"]: genre["name"] for genre in genre_response["genres"]}
 print(f"Genres fetched: {len(genres_map)} genres")
 
-print("Collecting data from TMDB...")
+print("\nCollecting data from TMDB...")
 
 for page in range(1,51): # Collecting 1000 popular movies
     url = f"{base_url}/movie/popular?api_key={api_key}&page={page}"
@@ -31,14 +33,9 @@ for page in range(1,51): # Collecting 1000 popular movies
                "genres": ",".join([genres_map.get(g_id, "Unknown") for g_id in movie.get("genre_ids", [])] or ["Unknown"]),
                "release_year": int(movie["release_date"][:4]) if movie["release_date"] else 0,
                "overview": movie["overview"] or "No description"
-           }) 
-
+           })
 df = pd.DataFrame(movies)
+print(f"\nSuccessfully collected {len(df)} movies")
 
-engine = sa.create_engine("mysql+mysqlconnector://root:12345678@localhost/movies_db")  # Replace with your credentials
+engine = sa.create_engine("mysql+mysqlconnector://root:12345678@localhost/movies_db") 
 df.to_sql("movies", engine, if_exists="replace", index=False)
-
-def get_genre(movie_id: int):
-    pass
-
-print(f"Length of the Movies dataframe: {len(df)}")
